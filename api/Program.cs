8c221bcb82232de60cd3b2ee81e905e1de362c9d
Dina Berry <diberry@microsoft.com>
2025-01-14 16:35:32 +0000

    Migrate API: .NET 6 -> .NET 9

--- api/Program.cs
+++ api/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWebApplication()
     .Build();
 
 host.Run();
